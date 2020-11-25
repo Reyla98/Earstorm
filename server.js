28a3d10f64a21b0d169ad8b21911b9b37e9aef26
Romain Carlier <romain.carlier2@gmail.com>
2020-11-25 11:49:20 +0100

    add mongoConnection

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var consolidate = require('consolidate');
 var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
 const port = 8080;
 
 //Set up server
@@ -12,19 +13,24 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 
-//Redirect
-app.get('/homepage', function(req,res){
-    res.render('homepage.html');
-});
+MongoClient.connect('mongodb://localhost:27017', function(err, db){
 
-app.post('/login', function(req,res){
-    res.render('homepage.html');
-});
+    dbo = db.db('earstorm');
 
-app.post('/signup', function(req,res){
-    res.render('homepage.html');
-});
+    app.get('/homepage', function(req,res){
+        res.render('homepage.html');
+    });
 
-app.listen(port, function(){
-    console.log('Server running on port 8080');
+    app.post('/login', function(req,res){
+        res.render('homepage.html');
+    });
+
+    app.post('/signup', function(req,res){
+        res.render('homepage.html');
+    });
+
+    app.listen(port, function(){
+        console.log('Server running on port 8080');
+    });
+
 });
