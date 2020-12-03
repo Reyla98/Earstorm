9b782730656279c061c178d28fbdd7b9f92c386c
im-number3 <73306464+im-number3@users.noreply.github.com>
2020-12-03 12:16:01 +0100

    atlas db

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var consolidate = require('consolidate');
 var bodyParser = require('body-parser');
-var MongoClient = require('mongodb').MongoClient;
 const port = 8080;
 let session = require('express-session');
 
@@ -15,6 +14,11 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 
+//Database 
+var MongoClient = require('mongodb').MongoClient;
+var uri = "mongodb+srv://groupD:group-5678D@earstorm.twelv.mongodb.net/Earstorm?retryWrites=true&w=majority";
+var client = new MongoClient(uri, { useNewUrlParser: true });
+var dbName = "earstorm"
 
 app.get('/homepage', function(req,res){
     if(req.session.username!=null){
@@ -38,17 +42,63 @@ app.post('/login', function(req,res){
     res.render("account.html", {username:req.session.username})
 });
 
-app.post('/signup', function(req,res){
-    req.session.username = req.body.signUpUsername;
-    res.render("account.html", {username:req.session.username});
+app.post('/signup', function(req,res){ //protect password with encryption
+	let user = req.body.signUpUsername;
+	let pwd = req.body.signUpPassword;
+	let mail = req.body.emailAddress;
+	req.session.username = req.body.signUpUsername;
+	let userInfo = {
+		user,
+		pwd,
+		mail
+	}
+	newUser(userInfo).catch(console.dir);
+  res.render("account.html", {username:req.session.username});
 });
 
 app.get('/addPlaylist', function(req,res){
     res.render('create_pl.html', {username:req.session.username});
 });
 
+// use double slash to test without some particular field(s) (do it before variable declaration + in playlistInfo)
 app.post('/createPlaylist', function(req,res){
-    res.redirect('/homepage');
+	let illustration = req.body.customFile
+	let name = req.body.playlist_name;
+	let creator = req.session.username;
+	let creation_date = new Date();
+	let mod_date = new Date();
+	let genres = (req.body.playlist_genres).replace(/&/g, '').split("playlist_genres");
+	let titles = (req.body.playlist_titles).split(", ");
+	let playlistInfo = {
+		illustration,
+		name,
+		creator,
+		creation_date,
+		mod_date,
+		genres,
+		titles
+  }
+	add(playlistInfo).catch(console.dir);
+	res.redirect("/homepage");
+});
+
+app.post('/modifyPlaylist', function(req,res){
+	//récupérer le "_id" de l'élément de la collection "playlists"
+	//can't modify creation_date & creator
+	let illustration = req.body.customFile
+	let name = req.body.playlist_name;
+	let mod_date = new Date();
+	let genres = (req.body.playlist_genres).replace(/&/g, '').split("playlist_genres");
+	let titles = (req.body.playlist_titles).split(", ");
+	let playlistInfo = {
+		illustration,
+		name,
+		mod_date,
+		genres,
+		titles
+  }
+	mod(id, playlistInfo).catch(console.dir);
+	res.redirect("/homepage");
 });
 
 app.get('/searchplaylist', function(req,res){
@@ -69,3 +119,72 @@ app.get('/logout', function(req,res){
 app.listen(port, function(){
     console.log('Server running on port 8080');
 });
+
+async function findID(pltitle) {
+	try {
+		await client.connect();
+		console.log("Connected correctly to server");
+		const db = client.db(dbName);
+		const col = db.collection("playlists");
+		const PL_ID = await col.find({"name":pltitle},{"name":0,"creation_date":0, "mod_date":0});
+		console.log(PL_ID);
+			
+	} catch (err) {
+		console.log(err.stack);
+		
+	} finally {
+		await client.close();
+	}
+}
+
+async function add(playlistInfo) {
+	try {
+		await client.connect();
+		console.log("Connected correctly to server");
+		const db = client.db(dbName);
+		const col = db.collection("playlists");
+		const p = await col.insertOne(playlistInfo);
+		console.log("New playlist created");
+			
+	} catch (err) {
+		console.log(err.stack);
+		
+	} finally {
+		await client.close();
+	}
+}
+
+//à tester quand on saura comment renvoyer l'id depuis le lien pour modifier la playlist
+async function mod(id, playlistInfo) {
+	try {
+		await client.connect();
+		console.log("Connected correctly to server");
+		const db = client.db(dbName);
+		const col = db.collection("playlists");
+		const p = await col.update({_id:id},{$set:{playlistInfo}});
+		console.log("Playlist updated");
+			
+	} catch (err) {
+		console.log(err.stack);
+		
+	} finally {
+		await client.close();
+	}
+}
+
+async function newUser(userInfo) {
+	try {
+		await client.connect();
+		console.log("Connected correctly to server");
+		const db = client.db(dbName);
+		const col = db.collection("users");
+		const p = await col.insertOne(userInfo);
+		console.log("New user created");
+	
+	} catch (err) {
+		console.log(err.stack);
+	
+	} finally {
+		await client.close();
+	}
+}
