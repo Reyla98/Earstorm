ed325329165231d5d0f4bd4a40172bc146b36bfa
laurane <laurane.castiaux@gmail.com>
2020-12-04 12:35:08 +0100

    improve plalist_content display

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var consolidate = require('consolidate');
-var bodyParser = require('body-parser');
-var MongoClient = require('mongodb').MongoClient;
+const express = require('express');
+const consolidate = require('consolidate');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 const port = 8080;
-var session = require('express-session');
-var bcrypt = require('bcrypt');
+const session = require('express-session');
+const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 //Set up server
-var app = express();
+const app = express();
 app.use(session({secret:'earstorm123'}));
 app.engine('html', consolidate.hogan);
 app.set('views', 'static');
@@ -19,8 +19,8 @@ app.use(bodyParser.text());
 
 
 MongoClient.connect('mongodb+srv://groupD:group-5678D@earstorm.twelv.mongodb.net/Earstorm?retryWrites=true&w=majority', { useUnifiedTopology: true }, (err, db)=>{
-  if (err) throw err;
-  var dbo = db.db('earstorm');
+    if (err) throw err;
+    var dbo = db.db('earstorm');
 	
 	app.get('/', function(req, res) {
 		res.redirect('/homepage');
@@ -76,7 +76,7 @@ MongoClient.connect('mongodb+srv://groupD:group-5678D@earstorm.twelv.mongodb.net
 				if(err) throw err;
 				if(result!=null){
 						res.render('login.html', {signupErrorMessage:"Username already taken"});
-				} else{
+				} else {
 						let username = req.body.signUpUsername;
 						let password = req.body.signUpPassword;
 						let email = req.body.emailAddress;
@@ -92,31 +92,31 @@ MongoClient.connect('mongodb+srv://groupD:group-5678D@earstorm.twelv.mongodb.net
 						res.render("account.html", {"username":username});
 				}
 		});
-  });
+    });
 	
 	app.get('/addPlaylist', function(req, res) {
 			res.render('create_playlist.html', {username:req.session.username});
     });
     
     app.get('/modifyPlaylist', function(req, res) {
+        // faire en sorte qu'on sache de quelle playlist il s'agit
         req.session.id = null;
         res.render('create_playlist.html', {username:req.session.username});
     });
 	
-  app.post('/createPlaylist', function(req, res) {
+    app.post('/createPlaylist', function(req, res) {
 		let illustration = req.body.customFile;
         let title = req.body.playlist_name;
         let description = req.body.playlist_descr || "No description";
 		let creator = req.session.username;
 		let creation_date = getFullDate();
-		let mod_date = getFullDate();
+		let modification_date = getFullDate();
 
         if (Array.isArray(req.body.playlist_genres)) {
             var genres = req.body.playlist_genres; //jsp pq mais ça marche pas avec let
         } else {
             var genres = [];                       //idem
             genres.push(req.body.playlist_genres);
-
         }
 
         let otherGenres = (req.body.playlist_add_genre).replace(/ /g, "").split(",");
@@ -124,31 +124,45 @@ MongoClient.connect('mongodb+srv://groupD:group-5678D@earstorm.twelv.mongodb.net
             genres.push(otherGenres[i]);
         }
 
-		let titles = (req.body.playlist_titles).replace(/ /g, "").split(",");
+		let urls = (req.body.playlist_titles).replace(/ /g, "").split(",");
 
-		if (req.session.playlist_id == null){
+		if (req.session.playlist_id == null) {
+            let songs = [];
+            for (let url of urls) {
+                songs.push({'url': url, 'date': getFullDate()})
+            }
 			let playlistInfo = {
-				picture : null,
-                title : title,
-                description : description,
-				creator : creator,
-				creation_date : creation_date,
-				modification_date : mod_date,
-				genres :genres,
-				titles : titles
+				picture: null,
+                title: title,
+                description: description,
+				creator: creator,
+				creation_date: creation_date,
+				modification_date: modification_date,
+				genres: genres,
+				songs: songs
 			};
 			dbo.collection('playlists').insertOne(playlistInfo, function(err,result) {
 				if(err) throw err;
 					console.log('Playlist added successfully');
 				});
         } else {
+            let songs = req.session.songs;
+            let url_already_in = {};
+            for (let song of songs) {
+                url_already_in.push(song.url);
+            }
+            for (let url of urls) {
+                if (! url_already_in.includes(url)) {
+                    songs.push({url: url, date: getFullDate()})
+                }
+            }
 			let playlistInfo = {
-				picture : null,
-                title : title,
-                description : description || "No description",
-				modification_date : mod_date,
-				genres :genres,
-				titles : titles
+				picture: null,
+                title: title,
+                description: description || "No description",
+				modification_date: mod_date,
+				genres: genres,
+				songs: songs
 			};
 			let id = req.session.playlist_id;
 			dbo.collection('playlists').updateOne({_id:id}, {$set:{playlistInfo}}, function(err, result) {
@@ -165,7 +179,7 @@ MongoClient.connect('mongodb+srv://groupD:group-5678D@earstorm.twelv.mongodb.net
         dbo.collection('playlists').findOne({title:query_title}, function(err, doc) {
             if (err) throw err;
             if (req.session.username != null) {
-                res.render("playlist_content.html", {"song_list": doc,
+                res.render("playlist_content.html", {"song_list": doc.songs,
                                              username: req.session.username,
                                              title: doc.title,
                                              genres: doc.genres,
@@ -173,7 +187,7 @@ MongoClient.connect('mongodb+srv://groupD:group-5678D@earstorm.twelv.mongodb.net
                                              creator: doc.creator
                                          });
             } else {
-                res.render('playlist_content.html', {"song_list": doc,
+                res.render('playlist_content.html', {"song_list": doc.songs,
                                                      login:"Log in",
                                                      title: doc.title,
                                                      genres: doc.genres,
@@ -204,7 +218,7 @@ MongoClient.connect('mongodb+srv://groupD:group-5678D@earstorm.twelv.mongodb.net
 	
 });
 
-function getFullDate(){
+function getFullDate() {
     let date = new Date();
     let d = date.getDate() + "/" + (Number(date.getMonth())+1).toString() +"/" + date.getFullYear();
     return d;
