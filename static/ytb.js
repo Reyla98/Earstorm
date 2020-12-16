614448a7d2160119b2f4bcdef088796dce1bf84a
Romain Carlier <romain.carlier2@gmail.com>
2020-12-16 18:56:06 +0100

    add autoplay if only ytb links

--- static/ytb.js
+++ static/ytb.js
@@ -5,62 +5,72 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 var player;
 var ids = getvidID();
+var sources = getSources();
 var i = 0;
-var rows = document.getElementById('songTable').getElementsByTagName('tr');
-rows[1].style.fontWeight = "bold";
-rows[1].style.fontSize = "1.15em";
-for(j=0; j<ids.length; j++){
-    let button = document.getElementById(ids[j]);
-    button.addEventListener("click", function(){
-    for(let i=1; i<rows.length; i++){
-        rows[i].style.fontWeight = "normal";
-        rows[i].style.fontSize = "1em";
-    }
-    i = ids.indexOf(button.id);
-    player.loadVideoById(ids[i]);
-    player.playVideo();
-    rows[i+1].style.fontWeight = "bold";
-    rows[i+1].style.fontSize = "1.15em";
-    });
-}
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-    height: '250',
-    width: '320',
-    videoId : ids[0],
-    events: {
-        'onReady' : onPlayerReady,
-        'onStateChange': onPlayerStateChange
-    }
-    });
-}
-function onPlayerReady(event){
-}
-function onPlayerStateChange(event) {
-    if (event.data == 0){
-    i++;
-    let prev = i
-    if(i>=ids.length){
-        i = 0;
-        prev = ids.length;
-    }
-    player.loadVideoById(ids[i]);
-    player.playVideo();
-    rows[prev].style.fontWeight = "normal";
-    rows[prev].style.fontSize = "1em";
-    rows[i+1].style.fontWeight = "bold";
-    rows[i+1].style.fontSize = "1.15em";
+var onlyYtb = true;
+for(i=0;i<sources.length;i++){
+    if (sources[i]!="youtube"){
+        onlyYtb = false;
     }
 }
-function stopVideo() {
-    player.stopVideo();
+if(onlyYtb){
+    var iframe = document.getElementById('song_player');
+    iframe.style.width = 0;
+    iframe.style.height = 0;
+    for(j=0; j<ids.length; j++){
+        let button = document.getElementById(ids[j]);
+        button.addEventListener("click", function(){
+            console.log('button')
+            i = ids.indexOf(button.id);
+            player.loadVideoById(ids[i]);
+            player.playVideo();
+        });
+    }
+
+    function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+        height: '250',
+        width: '320',
+        videoId : ids[0],
+        events: {
+            'onReady' : onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+        });
+    }
+    function onPlayerReady(event){
+    }
+    function onPlayerStateChange(event) {
+        if (event.data == 0){
+        i++;
+        let prev = i
+        if(i>=ids.length){
+            i = 0;
+            prev = ids.length;
+        }
+        player.loadVideoById(ids[i]);
+        player.playVideo();
+        }
+    }
+    function stopVideo() {
+        player.stopVideo();
+    }
 }
 
 function getvidID(){
-    var as = document.getElementsByTagName('a');
+    var as = document.getElementsByTagName('button');
     var ids = []
     for(i=3;i<as.length; i++){
-    ids.push(as[i].id);
+        ids.push(as[i].id);
     }
     return ids;
+}
+
+function getSources(){
+    var as = document.getElementsByTagName('button')
+    var sources = []
+    for(i=3;i<as.length;i++){
+        sources.push(as[i].name);
+    }
+    return sources;
 }
